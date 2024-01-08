9dfd0f9a17fb214d558fde8906296a2402b60493
Uzair Saleem <uzairsaleemdev@gmail.com>
2024-01-08 19:53:26 +0500

    Added particles-ts functionality

--- src/App.js
+++ src/App.js
@@ -2,37 +2,19 @@ import React from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageURLInput from "./components/ImageURLInput/ImageURLInput";
-import Particles from "react-particles-js";
+// Particles from "react-particles-js";
 import Rank from './components/Rank/Rank';
-
-
-const particlesOptions={
-  particles:{
-    number:{
-      value:30,
-      density:{
-        enable:true,
-        value_area:800
-      }
-    }
-  }
-}
-class App extends React.Component{
-
-render(){
-  return (
-    <div>
-    <Particles className="particles" params={particlesOptions}>
+import Particle from "./components/Particles/Particle";
+const App = () => {
+    return (
+      <div> 
+        <Particle/>
         <Navigation/>
         <Logo/>
         <Rank/>
         <ImageURLInput/>
-    </Particles>
-
-    </div>
-  )
-}
-}
+      </div>
+    );
+};
 
 export default App;
-
